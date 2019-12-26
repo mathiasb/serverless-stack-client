a9ffbfda44c2772ca152411045aa93ba7ef18a17
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2019-12-26 09:52:47 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-north-1",
+    BUCKET: "mathias-notes-app-tutorial"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://wtk68lx9e1.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_R7RZjXeJs",
+    APP_CLIENT_ID: "7o6q5dkk1rdve6qo10bs92tg86",
+    IDENTITY_POOL_ID: "us-east-1:b48fa73a-96f4-41c6-9e6a-3a38dee607c4"
+  }
+};
